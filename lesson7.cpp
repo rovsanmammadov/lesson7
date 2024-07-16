ecd8233d820405a480f0db3ddd256b33897eba08
Memme_dx48 <Memme_dx48@STHQ0305-05>
2024-07-16 18:40:11 +0400

    Add project files.

---
+++ lesson7.cpp
@@ -0,0 +1,106 @@
+	
+
+#include <iostream>
+
+using namespace std;
+
+
+enum  Mezenneler {Avro=1 , Dollar , Rubl};
+
+void main()
+{
+
+	//Task1
+
+	//cout << "Task1" <<endl;
+
+
+	//int azn = 0;
+	//Mezenneler pul;
+	//cout << "Azn ile meblegi daxil edin :";
+	//cin >> azn;
+	//cout << "Seciminizi daxil edin  (1-Avro , 2-Dollar , 3-Rubl) :";
+	//int secim;
+	//cin >> secim;
+	//Mezenneler mezenne = (Mezenneler)secim;
+
+	//float cevrilmis_avro = 0;
+	//float cevrilmis_dollar = 0;
+	//float cevrilmis_rubl = 0;
+
+
+	//switch (mezenne)
+	//{
+	//case Mezenneler::Avro:
+	//	cevrilmis_avro = azn / 1.8;
+	//	cout << cevrilmis_avro << endl;
+	//	break;
+	//case Mezenneler::Dollar:
+	//	cevrilmis_dollar = azn / 1.7;
+	//	cout << cevrilmis_dollar << endl;
+	//	break;
+	//case Mezenneler::Rubl:
+	//	cevrilmis_rubl =  azn / 51.99;
+	//	cout << cevrilmis_rubl << endl;
+	//	break;
+	//default:
+	//	cout << "Duzgun reqem daxil etmemisiniz";
+	//	break;
+	//}
+
+
+
+	//Task3
+
+	//cout << "\n\n\nTask3" << endl;
+	//cout << "Qiymeti daxil edin :";
+	//float qiymet;
+	//cin >> qiymet;
+	//cout << "Qrami daxil edin :";
+	//float qram;
+	//cin >> qram;	
+	//float hesab1 = qiymet * qram;
+	//float totalPrice;
+
+	//float hesab2 = (qiymet * qram) * 97 / 100;
+
+	//float hesab3 = (qiymet * qram) * 95 / 100;
+
+	//float hesab4 = (qiymet * qram) * 93 / 100;
+
+	//if (qram<100) {
+	//	totalPrice = hesab1;
+	//	cout << "Odeyeceyiniz mebleg---" <<hesab1 << endl;
+	//}
+	//else if (qram >= 100 && qram < 200) {
+	//	cout << "Odeyeceyiniz mebleg---" <<hesab2 << endl;
+	//}
+	//else if (qram >= 200 && qram < 300) {
+	//	cout << "Odeyeceyiniz mebleg---"<<hesab3 << endl;
+	//}
+	//else if (qram >= 300) {
+	//	cout << "Odeyeceyiniz mebleg---" << hesab4 << endl;
+	//}
+
+
+	//Task4
+
+	//cout << "\n\n\nTask4"<<endl;
+	//cout << "Kesr ededi daxil edin :";
+	//float kesr_eded;
+	//cin >> kesr_eded;
+	//
+	//if (kesr_eded<1 && kesr_eded>-1) {
+	//	cout << "Tam hissesi yoxdur";
+	//}
+	//else if (kesr_eded >= 1 || kesr_eded <= -1) {
+	//	cout << "Tam hissesi var";
+	//}
+
+
+
+
+
+}
+
+
